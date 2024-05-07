964ab534f679663ac16bedfb5e15fe956f9b1a33
georgebarwood <46846948+georgebarwood@users.noreply.github.com>
2024-05-07 16:08:26 +0100

    Add files via upload

--- benches/crit_bench.rs
+++ benches/crit_bench.rs
@@ -1,1 +1,52 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
+criterion_group!(benches, bench_get);
+criterion_main!(benches);
+
+/*
+fn bench_clone(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Clone");
+    for n in [1000, 10000].iter() {
+        let mut exp_map = btree_experiment::BTreeMap::new();
+        for i in 0..*n {
+            exp_map.insert(i, i);
+        }
+
+        let mut std_map = std::collections::BTreeMap::new();
+        for i in 0..*n {
+            std_map.insert(i, i);
+        }
+
+        group.bench_function(BenchmarkId::new("Exp", n), |b| b.iter(|| exp_map.clone()));
+        group.bench_function(BenchmarkId::new("Std", n), |b| b.iter(|| std_map.clone()));
+    }
+    group.finish();
+}
+*/
+
+fn bench_get(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Get");
+    for n in [100, 1000, 10000, 100000].iter() {
+        let mut exp_map = btree_experiment::BTreeMap::new();
+        for i in 0..*n {
+            exp_map.insert(i, i);
+        }
+
+        let mut std_map = std::collections::BTreeMap::new();
+        for i in 0..*n {
+            std_map.insert(i, i);
+        }
+
+        group.bench_function(BenchmarkId::new("Exp", n), |b| {
+            b.iter(|| exp_map.get(&(n - 100)))
+        });
+        group.bench_function(BenchmarkId::new("Std", n), |b| {
+            b.iter(|| std_map.get(&(n - 100)))
+        });
+    }
+    group.finish();
+}
+
+use mimalloc::MiMalloc;
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
